ee794b9fad1106dc3b8bb9e4ae520eec35f26107
chanelej <chanele01@live.fr>
2020-04-17 15:00:28 +0200

    ajout liste de tickets

---
+++ client/src/components/ListeTicket/index.tsx
@@ -0,0 +1,41 @@
+import React from 'react';
+import { FlatList} from 'react-native';
+import Item from "../../components/ListeTicketItem";
+
+const dataTicket = [
+    {
+        "id": "89375",
+        "commerce": "Carrefour",
+        "prix": "104,78",
+        "date": "20/01/2019"
+    },
+    {
+        "id": "11115",
+        "commerce": "Auchan",
+        "prix": "30,78",
+        "date": "20/01/2019"
+    },
+    {
+        "id": "45642",
+        "commerce": "Carrefour",
+        "prix": "24,78",
+        "date": "20/12/2020"
+    }
+]
+
+function TicketListe() {
+    return (
+        <view>
+            <FlatList
+                data={dataTicket}
+                keyExtractor={(item,index)=>item+index.toString()}
+                renderItem={({item}) => <Item  prix ={item.prix}
+                                                     commerce={item.commerce}
+                                                     date={item.date}
+                ></Item>}
+            />
+        </view>
+    );
+}
+
+export default TicketListe;
