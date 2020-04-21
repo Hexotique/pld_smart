968580ad0f1c02f39043692cf1ce25005eb4cc7c
Rahul Ramsaha <rahul.rahsaha@etic-insa.com>
2020-04-21 18:47:46 +0200

    Changement de auth vers client dans les méthodes correspondantes pour les classes routes>index,client et controller>clientController

--- server/src/routes/index.ts
+++ server/src/routes/index.ts
@@ -1,7 +1,7 @@
 import express from 'express';
 
-import authRoutes from './client';
+import clientRoutes from './client';
 
 export const init_rt = (app: express.Application) => {
-    app.use('/api/client', authRoutes);
+    app.use('/api/client', clientRoutes);
 }
