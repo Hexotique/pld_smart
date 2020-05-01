d390279e79e51c1445c8c311e77c76ef5f4fd7ff
teckwan <wong.teck.wan@outlook.com>
2020-05-01 06:01:01 +0200

    Finished retouch on appearance for shopping list

--- client/src/components/ItemListe/index.tsx
+++ client/src/components/ItemListe/index.tsx
@@ -1,45 +1,146 @@
 import React, { useRef, useMemo } from 'react';
-import { Animated, PanResponder, Text, View } from 'react-native';
+import { Animated, Dimensions, TouchableOpacity, Easing, PanResponder, Text, View } from 'react-native';
 import styles from './styles';
 
 type ItemListeProps = {
-    text: string;
-    toggleScroll: Function;
+    texte: string;
+    permetDefile: Function;
+    gauche?: any;
+    droite?: any;
+    gaucheHandler?: Function;
+    droiteHandler?: Function;
 };
 
 function ItemListe(props: ItemListeProps) {
-    const position = useRef(new Animated.ValueXY()).current;
+    const LARGEUR_ECRAN = Dimensions.get('window').width;
+    const SEUIL_MVT = LARGEUR_ECRAN / 15;
+    const SEUIL_VSN = LARGEUR_ECRAN / 7;
+    const SEUIL_ACT = LARGEUR_ECRAN / 4;
+
+    const DUREE_ANIM = 350;
+
+    const position = useRef(new Animated.ValueXY({ x: 0, y: 0 })).current;
+
+    const glisseComplete = (direction: string, cb: Function | undefined) => {
+        const x = direction === 'droite' ? LARGEUR_ECRAN : -LARGEUR_ECRAN;
+        Animated.timing(position, {
+            toValue: { x, y: 0 },
+            duration: DUREE_ANIM,
+            useNativeDriver: false
+        }).start()
+        if(cb) {
+            cb();
+        }
+    }
+
+    const montreBouton = (direction: string) => {
+        const x = direction === 'droite' ? LARGEUR_ECRAN / 4 : -LARGEUR_ECRAN / 4;
+        Animated.timing(position, {
+            toValue: { x, y: 0 },
+            duration: DUREE_ANIM,
+            easing: Easing.out(Easing.quad),
+            useNativeDriver: false
+        }).start(() => props.permetDefile(false));
+    }
+
+    const initPosition = () => {
+        Animated.timing(position, {
+            toValue: { x: 0, y: 0 },
+            duration: DUREE_ANIM,
+            useNativeDriver: false
+        }).start()
+    }
+
     const panResponder = useMemo(() => PanResponder.create({
-        onStartShouldSetPanResponder: (evt, gestureState) => false,
-        onMoveShouldSetPanResponder: (evt, gestureState) => true,
-        onPanResponderTerminationRequest: (evt, gestureState) => false,
-        onPanResponderMove: (evt, gestureState) => {
-            if (gestureState.dx < -35) {
-                props.toggleScroll(false);
-                let newX = gestureState.dx;
-                position.setValue({ x: newX, y: 0 });
+        onStartShouldSetPanResponder: (evt, geste) => false,
+        onMoveShouldSetPanResponder: (evt, geste) => true,
+        onPanResponderTerminationRequest: (evt, geste) => false,
+        onPanResponderGrant: () => {
+            position.setOffset({ x: (position.x as any)._value, y: 0 });
+            position.setValue({ x: 0, y: 0 });
+        },
+        onPanResponderMove: (evt, geste) => {
+            if (props.gauche && geste.dx >= SEUIL_MVT) {
+                props.permetDefile(false);
+                position.setValue({ x: geste.dx - SEUIL_MVT, y: 0 });
+            } else if (props.droite && geste.dx <= -SEUIL_MVT) {
+                props.permetDefile(false);
+                position.setValue({ x: geste.dx + SEUIL_MVT, y: 0 });
+            }
+
+        },
+        onPanResponderRelease: (evt, geste) => {
+            position.flattenOffset();
+            if (geste.dx > 0) {
+                if (props.gauche && geste.dx >= SEUIL_ACT) {
+                    glisseComplete('droite', props.droiteHandler)
+                } else if (props.gauche && geste.dx >= SEUIL_VSN) {
+                    montreBouton('droite');
+                } else {
+                    initPosition();
+                }
+            } else {
+                if (props.droite && geste.dx <= -SEUIL_ACT) {
+                    glisseComplete('gauche', props.gaucheHandler)
+                } else if (props.droite && geste.dx <= -SEUIL_VSN) {
+                    montreBouton('gauche');
+                } else {
+                    initPosition();
+                }
             }
         },
-        onPanResponderRelease: (evt, gestureState) => {
-            position.flattenOffset();
+        onPanResponderTerminate: () => {
+            Animated.spring(position, {
+                toValue: { x: 0, y: 0 },
+                useNativeDriver: false
+            }).start()
         }
     }), []);
 
+    const gauchePropStyle = () => {
+        const opacity = position.x.interpolate({
+            inputRange: [35, 75, 320],
+            outputRange: [0, 1, 0.25]
+        });
+        const width = position.x.interpolate({
+            inputRange: [0, 70],
+            outputRange: [0, 70]
+        });
+        return { opacity, width };
+    }
+
+    const droitePropStyle = () => {
+        const opacity = position.x.interpolate({
+            inputRange: [-LARGEUR_ECRAN, -120, -35],
+            outputRange: [0, 1, 0]
+        });
+        const width = position.x.interpolate({
+            inputRange: [-70, 0],
+            outputRange: [70, 0]
+        });
+        return { opacity, width };
+    }
+
     return (
-        <View style={styles.listItem} >
+        <View style={styles.conteneur} >
+            {props.gauche &&
+                <Animated.View style={[styles.conteneurGauche, gauchePropStyle()]}>
+                    {props.gauche}
+                </Animated.View>
+            }
             <Animated.View
-                style={[position.getLayout()]}
+                style={[styles.conteneurPrincipale, position.getLayout()]}
                 {...panResponder.panHandlers}
             >
-                <View style={styles.innerCell}>
-                    <Text style={styles.absoluteCellText}>
-                        {props.text}
-                    </Text>
-                </View>
-                <View style={styles.absoluteCell}>
-                    <Text style={styles.absoluteCellText}>DELETE</Text>
-                </View>
+                <Text style={styles.textePrincipale}>
+                    {props.texte}
+                </Text>
             </Animated.View>
+            {props.droite &&
+                <Animated.View style={[styles.conteneurDroite, droitePropStyle()]}>
+                    {props.droite}
+                </Animated.View>
+            }
         </View >
     );
 }
--- client/src/screens/ListeCourse/index.tsx
+++ client/src/screens/ListeCourse/index.tsx
@@ -1,12 +1,29 @@
-import React from 'react';
-import { SafeAreaView } from 'react-native';
+import React, { Fragment, useState } from 'react';
+import { SafeAreaView, Text, NativeSyntheticEvent, TextInputSubmitEditingEventData } from 'react-native';
 import ListeGlissable from '../../components/ListeGlissable';
 import Header from '../../components/ComposantsGénériques/Header';
 import { ListeCourseProp } from "../../navigator";
 import GestureRecognizer from 'react-native-swipe-gestures';
 import BarreNavigation from '../../components/ComposantsGénériques/BarreNavigation';
+import { TextInput } from 'react-native-gesture-handler';
 
 function ListeCourse({ route, navigation }: ListeCourseProp) {
+    const listData = [
+        { key: "Yaourts" },
+        { key: "Miel" },
+        { key: "Chips" }
+    ];
+
+    const [texteAjout, setTexteAjout] = useState("");
+    const [listeItems, setListeItems] = useState(listData);
+
+    const onSubmitHandler = (e: NativeSyntheticEvent<TextInputSubmitEditingEventData>) => {
+        let temp = listeItems.slice();
+        temp.unshift({ key: e.nativeEvent.text });
+        setListeItems(temp);
+        setTexteAjout("");
+    }
+
     return (
         <GestureRecognizer
             style={{ flex: 1 }}
@@ -14,7 +31,35 @@ function ListeCourse({ route, navigation }: ListeCourseProp) {
             onSwipeLeft={() => { navDroite(navigation) }}>
             <SafeAreaView style={{ flex: 1, backgroundColor: 'white' }}>
                 <Header indexe={1} />
-                <ListeGlissable />
+                <TextInput
+                    style={{
+                        height: 50,
+                        backgroundColor: "#fff2ee",
+                        textAlign: 'center'
+                    }} 
+                    placeholder="Ajoutez un item..."
+                    value={texteAjout}
+                    onChangeText={setTexteAjout}
+                    onSubmitEditing={onSubmitHandler}
+                />
+                <ListeGlissable
+                    items={listeItems}
+                    droite={(
+                        <Fragment>
+                            <Text
+                                numberOfLines={1}
+                                ellipsizeMode='clip'
+                                style={{
+                                    color: 'white',
+                                    fontWeight: 'bold',
+                                    fontFamily: 'Sans Serif'
+                                }}
+                            >
+                                SUPPRIMER
+                            </Text>
+                        </Fragment>
+                    )}
+                />
             </SafeAreaView>
             <BarreNavigation navGauche={() => navGauche(navigation)} navDroite={() => navDroite(navigation)} indexe={1} />
         </GestureRecognizer>
@@ -24,6 +69,7 @@ function ListeCourse({ route, navigation }: ListeCourseProp) {
 const navGauche = (nav: any) => {
     nav.navigate('GardeManger');
 }
+
 const navDroite = (nav: any) => {
     nav.navigate('ListeTicket')
 }
