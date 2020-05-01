2c08883b7b30f1691ee30c00c55e055b5cc4dbe3
Maxime <maximewoirin.97@orange.fr>
2020-05-01 18:52:29 +0200

    ajout reconnaissance code barre, affiche un pop up pour le moment

--- client/src/components/ScannerCodeBarre/index.tsx
+++ client/src/components/ScannerCodeBarre/index.tsx
@@ -4,12 +4,19 @@ import { RNCamera } from 'react-native-camera';
 import styles from './styles';
 
 
-function _codeBarreLu() {
-    console.log("code barre lu");
+interface DonneesCodeBarre {
+    data: any,
+    rawData: any,
+    type: any,
+    bounds: any
+}
+
+function _codeBarreLu(donnees: DonneesCodeBarre) {
+    Alert.alert("data : " + donnees.data + "\nrawData : " + donnees.rawData + "\ntype : " + donnees.type);
 }
 
 function ScannerCodeBarre() {
-    //const [torchOn, setTorchOn] = useState(false);
+    const [torchOn, setTorchOn] = useState(false);
 
     return (
         <View style={{ flex: 90 }}>
@@ -18,6 +25,7 @@ function ScannerCodeBarre() {
                 onBarCodeRead={_codeBarreLu}
                 type={RNCamera.Constants.Type.back}
                 captureAudio={false}
+                flashMode={torchOn ? RNCamera.Constants.FlashMode.on : RNCamera.Constants.FlashMode.off}
             >
             </RNCamera>
         </View>
