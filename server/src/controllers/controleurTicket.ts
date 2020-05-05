2ca6a4a439d550ca2d9bf007a05cd057f6e42037
Maxime <maximewoirin.97@orange.fr>
2020-05-05 22:39:00 +0200

    résolutions pb avec fetch sur le server

--- server/src/controllers/controleurTicket.ts
+++ server/src/controllers/controleurTicket.ts
@@ -4,7 +4,7 @@ import { json } from 'body-parser';
 import { Json } from 'sequelize/types/lib/utils';
 import sequelize, { Op } from 'sequelize';
 // 
-
+const fetch = require("node-fetch");
 
 interface DonneesMagasin {
     idCommerce: number;
@@ -426,8 +426,8 @@ export const creerArticle = async (code: string) => {
             (await cate?.addProduit(prod[0]));
         }
 
-        if (!prod[0].url_image){
-            prod[0].setAttributes({url_image : url_im});
+        if (!prod[0].url_image) {
+            prod[0].setAttributes({ url_image: url_im });
         }
 
         await (prod[0].addArticle(art));
